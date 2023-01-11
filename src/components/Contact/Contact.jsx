275200328e890d9c990d44935bf004a31fc120f6
Dheeraj Sharma <at60sharwan@gmail.com>
2023-01-11 06:44:13 +0530

    Create the mobile design for specific Phone only.

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,23 +6,23 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <section id="contacts">
-      <div className="container">
-        <div className="contact">
-          <h2 className="contact__heading">Contact</h2>
+    <section id='contacts'>
+      <div className='container'>
+        <div className='contact'>
+          <h2 className='contact__heading'>Contact</h2>
           <form>
             <div>
-              <input className="email" type="text" placeholder="Email" />
+              <input className='email' type='text' placeholder='Email' />
             </div>
-            <div className="">
+            <div className=''>
               <textarea
-                className="textarea"
-                placeholder="Meassage"
-                cols="30"
-                rows="10"
+                className='textarea'
+                placeholder='Meassage'
+                cols='10'
+                rows='10'
               ></textarea>
             </div>
-            <button className="submit__btn">Submit</button>
+            <button className='submit__btn'>Submit</button>
           </form>
         </div>
       </div>
